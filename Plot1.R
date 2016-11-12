84a63b5d19ea8b9a6390a9b5fbc138d35d66cc11
kalyansrinivasponnam <kalyan.p.srinivas@gmail.com>
2016-11-12 11:23:28 -0500

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,23 @@
+setwd("C:/Users/ks6552/Desktop/Rexercise")
+householdFile <- "C:/Users/ks6552/Desktop/Rexercise/household_power_consumption.txt"
+getwd()
+##
+if (!file.exists(householdFile)) {
+
+        temp <- tempfile()
+        download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+        unzip(temp, "household_power_consumption.txt")
+        plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
+        unlink(temp)
+        }
+
+##
+
+## set time variable
+finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalData <- cbind(SetTime, finalData)
+##
+## Generating Plot 1
+hist(finalData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
---
+++ Plot2.R
@@ -0,0 +1,25 @@
+setwd("C:/Users/ks6552/Desktop/Rexercise")
+householdFile <- "C:/Users/ks6552/Desktop/Rexercise/household_power_consumption.txt"
+getwd()
+##
+if (!file.exists(householdFile)) {
+
+        temp <- tempfile()
+        download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+        unzip(temp, "household_power_consumption.txt")
+        plotData <- read.table(householdFile, header=T, sep=";", na.strings="?")
+        unlink(temp)
+        }
+
+##
+
+## set time variable
+finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalData <- cbind(SetTime, finalData)
+##
+
+
+## Generating Plot 2
+plot(finalData$SetTime, finalData$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)")
+
